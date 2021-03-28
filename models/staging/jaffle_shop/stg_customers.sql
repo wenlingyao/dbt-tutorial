2458955db494c0c5823ff65d5e1c7d0ce3d8fb0a
Wenling <wenling_yao@outlook.com>
2021-03-28 01:07:15 +0100

    Add seeds, tests and refactor staging models to be based on seeds

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -2,9 +2,27 @@
   materialized="table"
 )}}
 
-select
-    id as customer_id,
-    first_name,
-    last_name
 
-from {{ source('jaffle_shop', 'customers')}}
+-- This referes to the table created from data/employees.csv
+-- Run `dbt seed` first to make sure the seeds data is loaded before `dbt run`
+with employees as (
+  select * from {{ ref('employees') }}
+),
+
+customers as (
+  select
+      id as customer_id,
+      first_name,
+      last_name
+
+  from {{ source('jaffle_shop', 'customers')}}
+),
+
+final as (
+  select
+      *
+  from customers
+  left join employees using (customer_id)
+)
+
+select * from final
